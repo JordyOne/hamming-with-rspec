dcfac39eb89715b8a469601bf442b3cfb6409621
Jordan Dale <Jordan.T.Dale@gmail.com>
2014-06-26 10:14:25 -0600

    All but the last test completed

--- lib/dna_strand_pair.rb
+++ lib/dna_strand_pair.rb
@@ -1,3 +1,34 @@
 class DnaStrandPair
 
+  def initialize(strand_1, strand_2)
+    @strand_1 = strand_1
+    @strand_2 = strand_2
+  end
+
+#
+
+  def hamming_distance
+    hamming_distance = 0
+
+    if @strand_1 >= @strand_2
+      @strand_1.each_char do |letter|
+        x = @strand_1.index(letter)
+        if letter != @strand_2[x]
+          hamming_distance = hamming_distance + 1
+        end
+      end
+
+    elsif @strand_2 > @strand_1
+      @strand_2.each_char do |letter|
+        x = @strand_2.index(letter)
+        if letter != @strand_1[x]
+          hamming_distance = hamming_distance + 1
+        end
+      end
+    end
+    hamming_distance
+  end
 end
+
+
+
